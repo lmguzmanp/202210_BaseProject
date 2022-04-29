75ba522b3614cf7439115ff9a1b52adde4cac418
Luis Guzman <lm.guzmanp1@uniandes.edu.co>
2022-04-29 07:09:24 -0500

    Add total of vehicles per brand list
    Signed-off-by: Luis Guzman <lm.guzmanp1@uniandes.edu.co>

--- src/app/vehicle/vehicle-list/vehicle-list.component.ts
+++ src/app/vehicle/vehicle-list/vehicle-list.component.ts
@@ -10,11 +10,32 @@ import { Vehicle } from "../vehicle";
 export class VehicleListComponent implements OnInit {
   vehicles!: Vehicle[];
 
+  // The following type was needed to allow iteration over string index
+  vehiclesPerBrand: { [key: string]: { marca: string; quantity: number } } = {};
+
+  // The following type allows to organize ObjectOfObjects into ArrayOfObjects
+  totalsPerBrand: { marca: string; quantity: number }[] = [];
+
   constructor(private vehicleService: VehicleService) {}
+
+  addBrands(vehicles: Vehicle[]): void {
+    vehicles.forEach(({ marca }) => {
+      if (!this.vehiclesPerBrand[marca]) {
+        this.vehiclesPerBrand[marca] = { marca, quantity: 0 };
+      }
+      this.vehiclesPerBrand[marca].quantity += 1;
+    });
+
+    // Convert ObjectOfObjects to ArrayOfObjects
+    Object.keys(this.vehiclesPerBrand).map(marca => {
+      this.totalsPerBrand.push(this.vehiclesPerBrand[marca]);
+    });
+  }
 
   getVehicles() {
     this.vehicleService.getVehicles().subscribe(vehicles => {
       this.vehicles = vehicles;
+      this.addBrands(vehicles);
     });
   }
 
